694e2ee12c11d3f0424eb0a27b2c8e9610ca5e6e
HowieTKL <10776992+HowieTKL@users.noreply.github.com>
2025-04-01 20:25:51 -0400

    echo
    type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,18 @@ public class Main {
       String input = scanner.nextLine();
       if ("exit 0".equals(input)) {
         System.exit(0);
+      } else if (input.startsWith("echo")) {
+        String toEcho = input.substring(5);
+        System.out.println(toEcho);
+      } else if (input.startsWith("type")) {
+        String cmd = input.substring(5);
+        switch (cmd) {
+          case "echo", "type", "exit" -> System.out.println(cmd + " is a shell builtin");
+          default -> System.out.println(cmd + ": not found");
+        }
+      } else {
+        System.out.println(input + ": command not found");
       }
-      System.out.println(input + ": command not found");
     }
   }
 
