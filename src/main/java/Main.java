b17726d47a4e635f7ecc48c736f18794cd09b9e9
HowieTKL <10776992+HowieTKL@users.noreply.github.com>
2025-04-01 20:18:18 -0400

    exit 0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,9 @@ public class Main {
       System.out.print("$ ");
       Scanner scanner = new Scanner(System.in);
       String input = scanner.nextLine();
+      if ("exit 0".equals(input)) {
+        System.exit(0);
+      }
       System.out.println(input + ": command not found");
     }
   }
